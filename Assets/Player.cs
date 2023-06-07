0d1c66ead685da10f30638f19b81a57ffd4df404
Peter Weiser <peterweiser87@gmail.com>
2023-06-08 00:02:53 +0300

    added idle and move animation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region Components
+    public Animator anim { get; private set; }
+    #endregion
+    #region States
     public PlayerStateMachine stateMachine { get; private set; }
-
     public PlayerIdleState idleState { get; private set; }
-
     public PlayerMoveState moveState { get; private set; }
+    #endregion
 
     private void Awake()
     {
@@ -21,6 +24,7 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
+        anim = GetComponentInChildren<Animator>();
         stateMachine.Initialize(idleState);
     }
 
