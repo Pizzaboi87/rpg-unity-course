758545c9425bf9a8c2abc995be10b5547ed7c6c8
Peter Weiser <peterweiser87@gmail.com>
2023-06-08 22:55:20 +0300

    added player jump and fall

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [Header("Move info")]
     public float moveSpeed = 8f;
+    public float jumpForce = 12f;
 
     #region Components
     public Animator anim { get; private set; }
@@ -15,6 +16,8 @@ public class Player : MonoBehaviour
     public PlayerStateMachine stateMachine { get; private set; }
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
+    public PlayerJumpState jumpState { get; private set; }
+    public PlayerAirState airState { get; private set; }
     #endregion
 
     private void Awake()
@@ -22,8 +25,9 @@ public class Player : MonoBehaviour
         stateMachine = new PlayerStateMachine();
 
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
-
         moveState = new PlayerMoveState(this, stateMachine, "Move");
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState = new PlayerAirState(this, stateMachine, "Jump");
     }
 
     private void Start()
