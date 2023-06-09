20ffddeaf1d50bb1791e041d759043fac9f55d4b
Peter Weiser <peterweiser87@gmail.com>
2023-06-09 23:22:26 +0300

    added player dash

--- Assets/PlayerGroundedState.cs
+++ Assets/PlayerGroundedState.cs
@@ -22,10 +22,10 @@ public class PlayerGroundedState : PlayerState
     {
         base.Update();
 
+        if (!player.IsGroundDetected())
+            stateMachine.ChangeState(player.airState);
+
         if (Input.GetKeyDown(KeyCode.Space) && player.IsGroundDetected())
             stateMachine.ChangeState(player.jumpState);
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            stateMachine.ChangeState(player.dashState);
     }
 }
