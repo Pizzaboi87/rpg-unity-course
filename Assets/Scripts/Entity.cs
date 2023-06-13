bac976c13cace43f4cf618b8b65e1aa869c86239
Peter Weiser <peterweiser87@gmail.com>
2023-06-13 22:13:17 +0300

    refactored player and added entity

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,18 +4,72 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    public virtual void Awake()
+    [Header("Collision info")]
+    [SerializeField] protected Transform groundCheck;
+    [SerializeField] protected float groundCheckDistance;
+    [SerializeField] protected Transform wallCheck;
+    [SerializeField] protected float wallCheckDistance;
+    [SerializeField] protected LayerMask whatIsGround;
+
+    public int facingDir { get; private set; } = 1;
+    protected bool facingRight = true;
+
+    #region Components
+    public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    #endregion
+
+    protected virtual void Awake()
     {
 
     }
 
-    public virtual void Start()
+    protected virtual void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Update()
     {
 
     }
 
-    public virtual void Update()
-    {
+    #region Velocity
+    public void ZeroVelocity() => rb.velocity = new Vector2(0, 0);
 
+    public void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rb.velocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
     }
+    #endregion
+
+    #region Collision
+    public virtual bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+    public virtual bool IsWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
+
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+    #endregion
+
+    #region Flip
+    public virtual void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public virtual void FlipController(float _x)
+    {
+        if (_x > 0 && !facingRight)
+            Flip();
+        else if (_x < 0 && facingRight)
+            Flip();
+    }
+    #endregion
 }
