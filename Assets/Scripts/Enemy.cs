3d4938baa334509431aefc793ecaf5a22581b13d
Peter Weiser <peterweiser87@gmail.com>
2023-06-14 22:00:38 +0300

    added first enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Entity
 {
-    public Rigidbody2D rb { get; private set; }
-    public Animator anim { get; private set; }
+    [Header("Move info")]
+    public float moveSpeed = 1.5f;
+    public float idleTime = 2f;
+
     public EnemyStateMachine stateMachine { get; private set; }
 
-    private void Awake()
+    protected override void Awake()
     {
+        base.Awake();
+
         stateMachine = new EnemyStateMachine();
     }
 
-    private void Update()
+    protected override void Update()
     {
+        base.Update();
+
         stateMachine.currentState.Update();
     }
 }
