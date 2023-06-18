d212e83b99998696ea5e93cfc0aedcdac8e39c07
Peter Weiser <peterweiser87@gmail.com>
2023-06-18 21:37:38 +0300

    started to add entity damage

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Entity : MonoBehaviour
 {
     [Header("Collision info")]
+    public Transform attackCheck;
+    public float attackCheckRadius;
     [SerializeField] protected Transform groundCheck;
     [SerializeField] protected float groundCheckDistance;
     [SerializeField] protected Transform wallCheck;
@@ -35,6 +37,11 @@ public class Entity : MonoBehaviour
 
     }
 
+    public virtual void Damage()
+    {
+        Debug.Log(gameObject.name + " was damaged.");
+    }
+
     #region Velocity
     public void SetZeroVelocity() => rb.velocity = new Vector2(0, 0);
 
@@ -53,6 +60,7 @@ public class Entity : MonoBehaviour
     {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
         Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+        Gizmos.DrawWireSphere(attackCheck.position, attackCheckRadius);
     }
     #endregion
 
